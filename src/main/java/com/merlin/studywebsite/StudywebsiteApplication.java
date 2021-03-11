b64df82214cd6499145965c921c4be12eb5aafcf
Merlin Dust <MLS1723978237@163.com>
2021-03-11 21:21:09 +0800

    first commit for testing

---
+++ src/main/java/com/merlin/studywebsite/StudywebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.merlin.studywebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudywebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudywebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merlin/studywebsite/StudywebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merlin.studywebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudywebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
